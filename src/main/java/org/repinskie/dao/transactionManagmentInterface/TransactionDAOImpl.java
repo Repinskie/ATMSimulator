99be048ba455e20c56b5dc8fbd4664097e6ad720
Michael Repinskiy <mihailrepinskie9999@icloud.com>
2024-03-26 19:26:47 +0400

    Feature: a method has been added to save transactions

--- src/main/java/org/repinskie/dao/transactionManagmentInterface/TransactionDAOImpl.java
+++ src/main/java/org/repinskie/dao/transactionManagmentInterface/TransactionDAOImpl.java
@@ -1,26 +1,32 @@
 package org.repinskie.dao.transactionManagmentInterface;
 
 
+import org.repinskie.dao.dbConnections.DBConnection;
+import org.repinskie.exception.DAOException;
 import org.repinskie.models.Transaction;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TransactionDAOImpl {
-    private Connection connection;
-
-    public TransactionDAOImpl(Connection connection) {
-        this.connection = connection;
+    private Transaction transaction;
+    public TransactionDAOImpl(Transaction transaction){
+        this.transaction = transaction;
     }
 
-    public void saveTransaction(Transaction transaction) {
-    }
-    /*public void save(Transaction transaction) throws SQLException{
-        String query = "INSERT INTO transactions (user_id,amount,type) VALUES (?,?,?)";
-       try(PreparedStatement statement = connection.prepareStatement(query)){
-           statement.setInt(1,transaction.getUserId());
-            statement.setBigDecimal(2,transaction.getAmount());
-            statement.setString(3,transaction.getType());
-            statement.executeUpdate();
+    public void saveTransaction(String senderName, String recipientName, double amount, Date transactionDate){
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO transactions (sender_name,recipient_name,amount,transaction_date) VALUES (?,?,?,?)"))
+       {
+           preparedStatement.setString(1,senderName);
+            preparedStatement.setString(2,recipientName);
+            preparedStatement.setDouble(3,amount);
+            preparedStatement.setDate(4,new java.sql.Date(transactionDate.getTime()));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            throw new DAOException(e);
         }
-    }*/
+    }
 }
